f55c4568887707007c460b5303d880788b513cc1
Amanda <aeverett1317@gmail.com>
2018-12-07 14:37:27 -0700

    created route for creating new users

--- server.js
+++ server.js
@@ -4,8 +4,23 @@ const bodyParser = require("body-parser");
 const port = process.env.PORT || 8000;
 // Use the array below to store the users. Add/remove/update items in it based off
 let storage = [];
+//{ "name":"user name", "email": "user email", "state": "CA" }
 app.use(bodyParser.json());
+//route for creating new users
+app.post('/users', (req, res) => {
+  let newUser = req.body
+  if(!newUser) {
+    res.sendStatus(400)
+  }
+  storage.push(req.body)
+  console.log(storage);
+  res.json(req.body.name)
+})
 
+//route for getting all users
+app.get('/guests', (req, res) => {
+
+})
 
 app.listen(port, ()=>{
   console.log(`Listening on port ${port}`);
